f447eedd639e2068d1138eee919bec6db6be83f0
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2020-12-03 13:38:36 -0400

    Added error and response verification in apiCode function in ViewController.swift

--- Backend-Workshop/Controller/ViewController.swift
+++ Backend-Workshop/Controller/ViewController.swift
@@ -25,59 +25,62 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    
 
     @IBAction func buttonTapped(_ sender: Any) {
         
     }
     
     func apiCode() {
-        //        if JSONSerialization.isValidJSONObject(validDictionary) { // True
-        //            do {
-        //                _ = try JSONSerialization.data(withJSONObject: validDictionary, options: .prettyPrinted)
-        //            } catch {
-        //                // Handle Error
-        //            }
-        //        }
-        //
-        //        if JSONSerialization.isValidJSONObject(invalidDictionary) { // False
-        //            // NSJSONSerialization.dataWithJSONObject(validDictionary, options: .PrettyPrinted) will produce an error if called
-        //        }
-        //        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
-        //        guard let url = URL(string: jsonUrlString) else { return }
-        //
-        //        URLSession.shared.dataTask(with: url) { (data, response, err) in
-        //            //perhaps check err
-        //            //also perhaps check response status 200 OK
-        //
-        //            guard let data = data else { return }
-        //
-        //        //            let dataAsString = String(data: data, encoding: .utf8)
-        //        //            print(dataAsString)
-        //
-        //            do {
-        //        //                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
-        //        //                print(websiteDescription.name, websiteDescription.description)
-        //
-        //                let courses = try JSONDecoder().decode([Course].self, from: data)
-        //                print(courses)
-        //
-        //                //Swift 2/3/ObjC
-        //        //                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
-        //        //
-        //        //                let course = Course(json: json)
-        //        //                print(course.name)
-        //
-        //            } catch let jsonErr {
-        //                print("Error serializing json:", jsonErr)
-        //            }
-        //
-        //
-        //
-        //        }.resume()
-        //
-        //        //        let myCourse = Course(id: 1, name: "my course", link: "some link", imageUrl: "some image url")
-        //        //        print(myCourse)
+        if JSONSerialization.isValidJSONObject(validDictionary) { // True
+            do {
+                _ = try JSONSerialization.data(withJSONObject: validDictionary, options: .prettyPrinted)
+            } catch {
+                // Handle Error
+            }
     }
+        
+        if JSONSerialization.isValidJSONObject(invalidDictionary) { // False
+            // NSJSONSerialization.dataWithJSONObject(validDictionary, options: .PrettyPrinted) will produce an error if called
+        }
+        
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                print("Error: \(error)")
+            }
 
+            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                print("Server error")
+                return
+            }
+            
+            guard let data = data else { return }
+        
+                //            let dataAsString = String(data: data, encoding: .utf8)
+                //            print(dataAsString)
+        
+            do {
+                //                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+                //                print(websiteDescription.name, websiteDescription.description)
+        
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                print(courses)
+        
+                        //Swift 2/3/ObjC
+                //                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+                //
+                //                let course = Course(json: json)
+                //                print(course.name)
+        
+            } catch let jsonErr {
+                print("Error serializing json:", jsonErr)
+            }
+        }.resume()
+        
+                //        let myCourse = Course(id: 1, name: "my course", link: "some link", imageUrl: "some image url")
+                //        print(myCourse)
+    }
 }
